7ed88b9cd415678dbc6b16db030dc00e16b87eca
Saulo Santos <saulo.correa@cast@.com.br>
2021-02-28 13:10:37 -0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/main/java/br/com/saulo/java/App.java,9/f/9f59ec2402111de2a038bc1612e32dbc45d164ed
---
+++ src/main/java/br/com/saulo/java/App.java
@@ -0,0 +1,15 @@
+package br.com.saulo.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        var i = 0;
+        System.out.println(i);
+    }
+}
